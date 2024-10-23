ec926cec8b8d4f35edb5f96548aab3d7bb7085b5
VicenteVargas1 <vice.vargasc@duocuc.cl>
2024-10-23 09:25:14 -0300

    api
    integracion de una api

--- src/app/vista1/vista1.page.ts
+++ src/app/vista1/vista1.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { WeatherService } from '../services/weather.service';
 
 @Component({
   selector: 'app-vista1',
@@ -9,19 +10,31 @@ import { Router } from '@angular/router';
 export class Vista1Page implements OnInit {
   nombre: string = '';
   apellido: string = '';
+  weatherData: any;
+  city: string = 'Santiago'; // Ciudad predeterminada o puedes usar la del usuario
 
-  constructor(private router: Router) {
-  }
+  constructor(private router: Router, private weatherService: WeatherService) {}
 
   ngOnInit() {
     const usuario = JSON.parse(localStorage.getItem('usuario') || '{}');
     if (usuario && usuario.email) {
       this.nombre = usuario.nombre;
       this.apellido = usuario.apellido;
+      this.getWeather();  
     } else {
-      // Redirige al usuario si no hay información en LocalStorage
+      
       this.router.navigate(['/home']);
     }
   }
 
+  getWeather() {
+    this.weatherService.getWeather(this.city).subscribe(
+      (data) => {
+        this.weatherData = data;
+      },
+      (error) => {
+        console.error('Error fetching weather data:', error);
+      }
+    );
+  }
 }
