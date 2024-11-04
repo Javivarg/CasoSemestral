01a72d3ae8098ce361cc8d4a873e639af193d078
Javivarg <javi.vargasv@duocuc.cl>
2024-11-03 23:25:54 -0300

    implementacion scanner qr
    se implementa scanner qr, pero no se prueba debido a que no se cuenata con equipo con camara

--- src/app/qr/qr.page.ts
+++ src/app/qr/qr.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-qr',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QrPage implements OnInit {
 
+  scannedData: string | null = null; // Variable para almacenar los datos escaneados
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async startScan() {
+    // Verifica y solicita permisos para acceder a la cámara
+    const permission = await BarcodeScanner.checkPermission({ force: true });
+    
+    if (permission.granted) {
+      // Oculta el fondo para que el escáner se ejecute en pantalla completa
+      BarcodeScanner.hideBackground();
+
+      const result = await BarcodeScanner.startScan(); // Inicia el escáner
+
+      // Verifica si hay contenido escaneado
+      if (result.hasContent) {
+        this.scannedData = result.content; // Guarda el contenido del código QR escaneado
+        console.log('Scanned data:', this.scannedData);
+      }
+
+      // Muestra el fondo nuevamente cuando termina el escaneo
+      BarcodeScanner.showBackground();
+    } else {
+      console.log('No se otorgaron permisos para usar la cámara.');
+    }
   }
 
+  // Método para detener el escáner (opcional)
+  stopScan() {
+    BarcodeScanner.stopScan();
+  }
 }
